2b6dad8b716168122f468289918a2a10738d257f
Hax Techs <mawuliakagbo589@gmail.com>
2025-08-04 23:27:21 +0000

    Rofi: added the 'simple' theme to the style menu as well

--- .config/rofi/scripts/rofi-launcher-picker.sh
+++ .config/rofi/scripts/rofi-launcher-picker.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-CHOICE=$(printf "Launcher 2 (type-2)" | rofi -dmenu -i -p "Choose Launcher Type")
+# Rofi theme file
+ROFI_DIR="$HOME/.config/rofi/launchers/styles"
+THEME='simple'
+
+CHOICE=$(printf "Launcher 2 (type-2)" | rofi -dmenu -i -p "Choose Launcher Type" -theme ${ROFI_DIR}/${THEME}.rasi)
 
 case "$CHOICE" in
   *2*) ~/.config/rofi/scripts/launcher_t2 ;;
